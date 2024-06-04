3f76d5aa1c9eb9573b5715a4acbd1035b74f29cc
ahmed-hady-org <ahmed.hady.dev@gmail.com>
2024-06-04 10:50:41 +0300

    add register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,28 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_button.dart';
 import 'package:food_delivery_app/components/my_text_field.dart';
-import 'package:food_delivery_app/pages/home_page.dart';
 
-class LoginPage extends StatefulWidget {
-  LoginPage({Key? key, this.onTap}) : super(key: key);
+class RegisterPage extends StatelessWidget {
+  RegisterPage({Key? key, this.onTap}) : super(key: key);
   final Function()? onTap;
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
-
   final TextEditingController passwordController = TextEditingController();
-  void login() {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const HomePage(),
-        ));
-  }
-
+  final TextEditingController confirmPasswordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,30 +17,32 @@ class _LoginPageState extends State<LoginPage> {
         children: <Widget>[
           Icon(Icons.lock_open_rounded, size: 100, color: Theme.of(context).colorScheme.inversePrimary),
           const SizedBox(height: 25),
-          Text('Food Delivery App',
+          Text('Let\'s Create Account for you',
               style: TextStyle(fontSize: 16, color: Theme.of(context).colorScheme.inversePrimary)),
           const SizedBox(height: 25),
           MyTextField(controller: emailController, hint: 'Email', obscureText: false),
           const SizedBox(height: 10),
           MyTextField(controller: passwordController, hint: 'Password', obscureText: true),
+          const SizedBox(height: 10),
+          MyTextField(controller: confirmPasswordController, hint: 'Confirm Password', obscureText: true),
           const SizedBox(height: 25),
           MyButton(
-            text: 'Sign In',
-            onTap: () => login(),
+            text: 'Sign Up',
+            onTap: () {},
           ),
           const SizedBox(height: 25),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                'Not a member?',
+                'Already have an account?',
                 style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
               ),
               const SizedBox(width: 4),
               GestureDetector(
-                onTap: widget.onTap,
+                onTap: onTap,
                 child: Text(
-                  'Register now',
+                  'Login here',
                   style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary, fontWeight: FontWeight.bold),
                 ),
               )
